c76c9b6b735fd927d48d7627f3f29f5b57c21dd3
thanhbt <buitrungthanh260897@gmail.com>
2021-09-20 15:47:40 +0700

    demo authentication email

--- ChatDemo/Controller/LoginViewController.swift
+++ ChatDemo/Controller/LoginViewController.swift
@@ -6,10 +6,19 @@
 //
 
 import UIKit
-
+import Firebase
+import FirebaseAuth
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var mailText: UITextField!
+    @IBOutlet weak var PassWordText: UITextField!
     @IBOutlet weak var LoginButton: UIButton!
+    // test signout
+    override func viewWillAppear(_ animated: Bool) {
+        if Auth.auth().currentUser != nil {
+            try!  Auth.auth().signOut()
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         LoginButton.setupButton()
@@ -43,6 +52,32 @@ class LoginViewController: UIViewController {
                 print(post)
             case .failure(let error):
                 print(error.localizedDescription)
+            }
+        }
+    }
+    @IBAction func ForgotPasswordButton(_ sender: Any) {
+    }
+    @IBAction func LoginButton(_ sender: Any) {
+    }
+    @IBAction func RegisterButton(_ sender: Any) {
+        if #available(iOS 13.0, *) {
+            let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "RegisterViewController") as! RegisterViewController
+            navigationController?.pushViewController(vc, animated: true)
+                                    } else {
+            // Fallback on earlier versions
+            }
+    }
+    
+    @IBAction func LoginButtontap(_ sender: Any) {
+        if let email = mailText.text, let password = PassWordText.text {
+            Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
+              //guard let strongSelf = self else { return }
+                if let _ = error {
+                    print("dang nhap khong thanh cong")
+                }
+                if Auth.auth().currentUser != nil{
+                    print("dang nhap thanh cong")
+                }
             }
         }
     }

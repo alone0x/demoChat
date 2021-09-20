24a6d2b7704d8ea1004ecc2936399733a1b342af
thanhbt <buitrungthanh260897@gmail.com>
2021-09-20 15:47:40 +0700

    demo authentication email

--- ChatDemo/Controller/RegisterViewController.swift
+++ ChatDemo/Controller/RegisterViewController.swift
@@ -6,24 +6,46 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class RegisterViewController: UIViewController {
-
+    @IBOutlet weak var NameText: UITextField!
+     
+    @IBOutlet weak var EmailTetxt: UITextField!
+    @IBOutlet weak var PassWordText: UITextField!
+    @IBOutlet weak var ButtonSignup: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        ButtonSignup.setupButton()
+//        FirebaseAuth.Auth.auth().signIn(withEmail: "", password: <#T##String#>, completion: <#T##((AuthDataResult?, Error?) -> Void)?##((AuthDataResult?, Error?) -> Void)?##(AuthDataResult?, Error?) -> Void#>)
+//        // Do any additional setup after loading the view.
     }
     
+    func isValidEmail(email: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: email)
     }
-    */
+
+    @IBAction func BackButton(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
+    }
+    @IBAction func RegisterButton(_ sender: Any) {
+        if let email = EmailTetxt.text , let password = PassWordText.text{
+        Auth.auth().createUser(withEmail: email , password: password) {[weak self] rerult, Error in
+            guard let self =  self else {
+                return
+            }
+            print("da dang ki")
+        }
+        }
+    //    FirebaseAuth.Auth.auth().signIn(withEmail: "", password: <#T##String#>, completion: <#T##((AuthDataResult?, Error?) -> Void)?##((AuthDataResult?, Error?) -> Void)?##(AuthDataResult?, Error?) -> Void#>)
+        
+    }
+    @IBAction func LogginButton(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
+    }
 
 }
